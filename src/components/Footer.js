361665d0c514c24626b262991d981fea505f7e8b
SATHRIYANLATHA <sathriyanlatha@gmail.com>
2024-09-07 01:57:06 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return (
+        <>
+             <footer className="py-1 bg-dark">
+      <h4 className="text-center text-white mt-1 ">
+        !....... Sathriyan Cart .......!
+      </h4>
+      <p>HAPPY SHOPPING  &#128522;</p>
+    </footer>
+        </>
+    )
+
+}
